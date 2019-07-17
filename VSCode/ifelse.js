d816975cda06ecbbd694de270bd8f12b1334cffc
Jigar Sony <sonijigar94@gmail.com>
2019-07-17 23:00:19 +0530

    17-July
    Added 3 Lec

---
+++ VSCode/ifelse.js
@@ -0,0 +1,10 @@
+let age = 18;
+
+if(age>18){
+    console.log("Congo");
+}else if(age == 18){
+    console.log("Average")
+}
+else{
+    console.log("Dingo");
+}
---
+++ VSCode/typeOperators.js
@@ -0,0 +1,22 @@
+/*********Type Operators */
+
+
+let firstName = "Jigar";
+
+console.log(typeof firstName);
+
+
+let i = 10;
+
+console.log(typeof i);
+
+console.log(typeof 123);
+console.log(typeof 123.34);
+console.log(typeof true);
+
+
+let lastName = new String("Sony");
+console.log(lastName instanceof String);
+
+let number1 = new Number(10);
+console.log(number1 instanceof Number);
